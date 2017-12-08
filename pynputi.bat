2d349a5481b1844eda9e570781e598fe895ea2d1
Yahavar <yahavar@github.com>
2017-12-09 00:00:39 +0200

    Finished deleting of word

---
+++ pynputi.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\Owner\AppData\Local\Programs\Python\Python36-32\Scripts
+pip install pynput
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyCorrector',
+    version='0.1',
+    packages=['pynput'],
+    url='',
+    license='',
+    author='Yahav Armerman',
+    author_email='yahav170602@gmail.com',
+    description=''
+)
