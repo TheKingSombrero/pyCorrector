af81759efcd59a5a4e3b0320b3dd15b9dab9b19b
yahav <yahav170602@gmail.com>
2021-04-05 01:16:30 +0300

    rewrite messy code and add documention vol. 1

--- Corrector.py
+++ Corrector.py
@@ -5,20 +5,20 @@ d = {}
 with open("file.txt") as f:
     for line in f:
         (key, val) = line.split()
-        d[(key)] = val  # Turning file to dictionary
+        d[key] = val  # Turning file to dictionary
 while True:
     keys = []
     word = ""
     i = 1
 
-    a = Controller()
+    controller_object = Controller() # init keyboard manager object
 
 
     def on_press(key):
         try:
-            print('alphanumeric key {0} pressed'.format(
-                key.char))
+            print('alphanumeric key {0} pressed'.format(key.char))  # log key pressed to console
             keys.append(key.char)
+            # If key entered is special i.e not alphanumeric print special key
         except AttributeError:
             print('special key {0} pressed'.format(
                 key))
@@ -37,16 +37,12 @@ while True:
             on_press=on_press,
             on_release=on_release) as listener:
         listener.join()
-    for key in keys:  # creating word
-        word += key
+    word = word.join(keys) #concat keys into word
 
-    if word in d.keys():
-        correction = d[word]
-        a.press(Key.backspace.value)
-        a.release(Key.backspace.value)
-        while i <= len(word):
-            a.press(Key.backspace.value)
-            a.release(Key.backspace.value)
-            i += 1
-        a.type(correction)
-        a.type(" ")
+    if word in d.keys():  # check if word exists in dictionary
+        correction = d[word]  # fetch correction from dict
+        for i in range(len(word)+1):  # delete entire word and space
+            controller_object.press(Key.backspace.value)
+            controller_object.release(Key.backspace.value)
+        controller_object.type(correction)  # write correction and space
+        controller_object.type(" ")
